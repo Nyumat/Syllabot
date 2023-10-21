cfd97242df444ac89ee32bccc5258af9bc753e07
ipoogleduck <ipoogleduck@gmail.com>
2023-10-21 14:57:02 -0700

    Justifies profile pic in center

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,4 @@
 import NavBar from "../components/NavBar";
-import CourseBtn from "../components/buttons/CourseBtn";
-import FileManageBtn from "../components/buttons/FileManageBtn";
-import ImageUploader from "../components/ImageUploader";
-import { FaPlus } from "react-icons/fa";
 import {
     SignedIn,
     SignedOut,
@@ -13,17 +9,12 @@ import CourseSidePanel from "../components/CourseSidePanel";
 export default function Home() {
     return (
         <>
-            <SignedIn>
-                <div className="text-center items-center justify-center content-center h-full font-body">
-                    <NavBar />
-                    <div className="grid grid-cols-3 gap-4">
-                        <CourseSidePanel />
-                    </div>
+            <div className="text-center items-center justify-center content-center h-full font-body">
+                <NavBar />
+                <div className="grid grid-cols-3 gap-4">
+                    <CourseSidePanel />
                 </div>
-            </SignedIn>
-            <SignedOut>
-                <Navigate to='../' />
-            </SignedOut>
+            </div>
         </>
     );
 }

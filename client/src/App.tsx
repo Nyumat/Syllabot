cbfa7ae01d675d0160f636d1e2d08f4e85ae02da
Thomas <nyumat18@gmail.com>
2023-10-20 19:29:14 -0700

    🥳 Vite + tailwind + tsx client added

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center text-blue-500">
+        Hello Vite + React!
+      </h1>
+    </>
+  );
+}
+
+export default App;

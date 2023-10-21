20d948879621bd10d81cd0ec434e49f84df446f6
ImgyeongLee <mingbab33@gmail.com>
2023-10-20 19:48:43 -0700

    Add Tailwindcss color extends

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,24 @@
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "#FDFDFF",
+        "primary-orange": "#DC4405",
+        "primary-black": "#252525",
+        "primary-grey": "#ECECEC",
+        white: "#FFFFFF",
+        black: "#000000",
+      },
+      fontFamily: {
+        general: ["Roboto"],
+      },
+    },
   },
   daisyui: {
     themes: [],
   },
   plugins: [daisyui],
-}
+};

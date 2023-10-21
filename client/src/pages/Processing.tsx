bc80a1a808f0ce47999b95888428b2d69fa014d2
ipoogleduck <ipoogleduck@gmail.com>
2023-10-21 14:29:23 -0700

    No nav on loading, and cleanup

--- client/src/pages/Processing.tsx
+++ client/src/pages/Processing.tsx
@@ -1,10 +1,10 @@
 import LoadingSpinner from "../components/spinners/LoadingSpinner";
-import NavBar from "../components/NavBar";
+// import NavBar from "../components/NavBar";
 
 export default function Processing() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <div className="text-center text-5xl font-bold mb-6 mt-10">
         Analyzing your syllabus...
       </div>

2b61dbe7e483661c077b60949775146362d7b390
Sankalp Patil <patilsa@oregonstate.edu>
2023-10-20 21:56:30 -0700

    basic file upload

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,11 +3,11 @@ import SignIn from "../components/modals/SignIn";
 
 const NavBar: React.FC = () => {
     return (
-        <nav className='nav p-4 max-h-12'>
+        <nav className='nav p-4 max-h-15 bg-slate-100'>
             <ul className='flex flex-row justify-between'>
                 <li className='mr-4'>
                     <a href="/">
-                        <figure className="h-36 w-36">
+                        <figure className="w-36">
                             <img src="/lightMode.svg" alt="canvas logo" className="rounded-xl" />
                         </figure>
                     </a>

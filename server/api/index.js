4c31b128bd21a71fe1fce2202a5b3ca449b4c13f
Sankalp Patil <bothellsandy@gmail.com>
2023-10-21 20:48:11 -0700

    Fixed route placement and imports

--- server/api/index.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require("express");
-
-const router = Router();
-
-router.use("/users", require("./users"));
-module.exports = router;
--- server/models/File.js
+++ server/models/File.js
@@ -22,4 +22,21 @@ const fileSchema = mongoose.Schema({
 
 const File = mongoose.model("File", fileSchema);
 
+const createFile = async (body) => {
+      try {
+            const newFile = new File({
+                  fileName: body.fileName,
+                  contentType: body.contentType,
+                  userId: body.userId,
+                  path: body.path,
+            });
+            await newFile.save();
+            console.log(`New file is successfully saved: ${newFile._id}`);
+            return newFile._id;
+      } catch (err) {
+            return null;
+      }
+};
+
+export { createFile };
 export default File;

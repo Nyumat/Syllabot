e02ae1034a98727ed20928fed87174c7ff49c490
Thomas <nyumat18@gmail.com>
2023-10-21 18:23:43 -0700

    🚂 Presetsss

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import {
 } from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
 import CourseSidePanel from "../components/CourseSidePanel";
+import Presets from "../components/lists/Presets";
 
 export default function Home() {
     const { isSignedIn } = useAuth();
@@ -25,7 +26,9 @@ export default function Home() {
             <div className="text-center items-center justify-center content-center h-full font-body">
                 <NavBar />
                 <div className="grid grid-cols-3 gap-4">
-                    <CourseSidePanel userClicked={(id: string) => console.log(id)} userClickedManageOrAdd={userClickedManage}/>
+                    <CourseSidePanel userClicked={(id: string) => console.log(id)} userClickedManageOrAdd={userClickedManage} />
+                    
+                    <Presets />
                 </div>
             </div>
         </>

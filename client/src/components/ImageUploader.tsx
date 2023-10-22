b6fcb11f2f71db57d60597bd880d8baddf78886c
Tom Nyuma <46255836+Nyumat@users.noreply.github.com>
2023-10-22 03:16:14 -0700

    😈 Processing State Navigation

--- client/src/components/ImageUploader.tsx
+++ client/src/components/ImageUploader.tsx
@@ -1,36 +1,67 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useDropzone } from "react-dropzone";
-import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
-export default function ImageUploader({ mini = false }) {
-  
+type Response = {
+  data: {
+    lc: number;
+    type: string;
+    id: string[];
+  };
+};
+
+interface ImageUploaderProps {
+  mini?: boolean;
+  isUploaded: boolean;
+  setHasUploaded: (hasUploaded: boolean) => void;
+}
+
+export default function ImageUploader(props: ImageUploaderProps) {
+  const navigate = useNavigate();
   const onDrop = (acceptedFiles: File[]) => {
     const data = new FormData();
-    data.append('file', acceptedFiles[0]);
-  
+    data.append("file", acceptedFiles[0]);
+    data.append("userId", uuid());
+    data.append("courseId", uuid());
+    props.setHasUploaded(true);
+
     const port = 8080; // Replace with your server's port
-    axios.post(`http://localhost:${port}/api/users/newFile`, data)
-      .then((res) => {
-        console.log(res.data); // Use res.data to access the server response
+    navigate("/processing");
+    axios
+      .post(`http://localhost:${port}/api/index`, data)
+      .then((res: Response) => {
+        const { lc, type, id } = res.data; // We can get other things if we want, eh.
+        console.log({ lc, type, id });
+        navigate("/chat");
       })
       .catch((error) => {
+        alert("Error uploading file");
         console.error(error);
+        navigate("/");
+        props.setHasUploaded(false);
       });
   };
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: {
-      'application/pdf': ['.pdf'], // Specify the accepted file type(s)
-    }
+      "application/pdf": [".pdf"], // Specify the accepted file type(s)
+    },
   });
 
+  useEffect(() => {
+    if (props.isUploaded) {
+      navigate("/");
+    }
+  }, [props.isUploaded, navigate]);
+
   return (
-    <div
-      {...getRootProps({ className: "dropzone content-center p-10" })}
-    >
+    <div {...getRootProps({ className: "dropzone content-center p-10" })}>
       <div className="flex flex-col items-center cursor-pointer">
         <input {...getInputProps()} />
-        {mini && <img src="/doc-icon.svg" />}
+        {props.mini && <img src="/doc-icon.svg" />}
         <button
           className="btn btn-primary normal-case bg-primary-orange cursor-pointer text-white hover:bg-white hover:text-primary-orange shadow border-none mt-8 rounded-2xl pr-7 pl-7 text-la"
           type="button"

b4822b96468ce2345744ffb62a8479bda5f411f4
Thomas <nyumat18@gmail.com>
2023-10-21 17:36:39 -0700

    🥹 WE DID IT.

--- server/lib/langchain.js
+++ server/lib/langchain.js
@@ -1,16 +1,50 @@
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
 /*
- * @param file (either string or blob)
+ * @param file (either string, Buffer, or Blob)
  * @return string
  */
-export default async function processPdf(file) {
-  const loader = new PDFLoader(file, {
-    splitPages: false,
-  });
+async function processPdf(file) {
+  let loader;
+
+  if (typeof file === "string") {
+    loader = new PDFLoader(file, {
+      splitPages: false,
+    });
+  } else if (Buffer.isBuffer(file)) {
+    const blob = new Blob([file]);
+    loader = new PDFLoader(blob, {
+      splitPages: false,
+    });
+  } else if (isBlob(file)) {
+    loader = new PDFLoader(file, {
+      splitPages: false,
+    });
+  } else {
+    throw new Error("Unsupported input type");
+  }
 
   const docs = await loader.load();
   const processedString = docs[0].pageContent.replace(/\n/g, " ");
 
   return processedString;
 }
+
+function isBlob(obj) {
+  return obj instanceof Blob;
+}
+
+async function readBlobAsArrayBuffer(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      resolve(event.target.result);
+    };
+    reader.onerror = (error) => {
+      reject(error);
+    };
+    reader.readAsArrayBuffer(blob);
+  });
+}
+
+export { isBlob, readBlobAsArrayBuffer, processPdf };

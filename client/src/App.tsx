8193a49d845f5f00904bd70fcabf50aa8bd444fe
ImgyeongLee <mingbab33@gmail.com>
2023-10-21 17:57:59 -0700

    Responsive Style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,24 +6,24 @@ import Processing from "./pages/Processing";
 import Home from "./pages/Home";
 
 if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+   throw new Error("Missing Publishable Key");
 }
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 function App() {
-  return (
-    <>
-      <ClerkProvider publishableKey={clerkPubKey}>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/processing" element={<Processing />} />
-          <Route path="/manage" element={<Manage />} />
-          <Route path="/chat" element={<Home />} />
-        </Routes>
-      </ClerkProvider>
-    </>
-  );
+   return (
+      <>
+         <ClerkProvider publishableKey={clerkPubKey}>
+            <Routes>
+               <Route path="/" element={<Landing />} />
+               <Route path="/processing" element={<Processing />} />
+               <Route path="/manage" element={<Manage />} />
+               <Route path="/chat" element={<Home />} />
+            </Routes>
+         </ClerkProvider>
+      </>
+   );
 }
 
 export default App;

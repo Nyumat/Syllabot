54bfd1abcc38c074f9fcc654c80e22697cef3671
ImgyeongLee <mingbab33@gmail.com>
2023-10-22 00:26:25 -0700

    minor css changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,8 @@ import Manage from "./pages/Manage";
 import Processing from "./pages/Processing";
 import Home from "./pages/Home";
 
-if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
-   throw new Error("Missing Publishable Key");
-}
-
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const clerkPubKey =
+   "pk_test_YWNjdXJhdGUtY295b3RlLTkyLmNsZXJrLmFjY291bnRzLmRldiQ";
 
 function App() {
    return (

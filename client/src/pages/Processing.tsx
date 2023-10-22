c970a21742af385bc45a7ec72a4db3ef858a0c2b
ImgyeongLee <mingbab33@gmail.com>
2023-10-21 18:09:43 -0700

    Add Header for Processing page

--- client/src/pages/Processing.tsx
+++ client/src/pages/Processing.tsx
@@ -1,17 +1,17 @@
 import LoadingSpinner from "../components/spinners/LoadingSpinner";
-// import NavBar from "../components/NavBar";
+import Header from "../components/Header";
 
 export default function Processing() {
-  return (
-    <>
-      {/* <NavBar /> */}
-      <div className="text-center text-5xl font-bold mb-6 mt-10">
-        Analyzing your syllabus...
-      </div>
-      <div className="flex place-content-center font-light text-2xl mb-20">
-        Something something something
-      </div>
-      <LoadingSpinner />
-    </>
-  );
+   return (
+      <>
+         <Header />
+         <div className="text-center text-5xl font-bold mb-6 mt-10">
+            Analyzing your syllabus...
+         </div>
+         <div className="flex place-content-center font-light text-2xl mb-20">
+            Something something something
+         </div>
+         <LoadingSpinner />
+      </>
+   );
 }

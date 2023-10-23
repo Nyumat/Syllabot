b634d443a792547dcfce46c2f52a731fd9ca9b12
ImgyeongLee <mingbab33@gmail.com>
2023-10-22 17:33:30 -0700

    Add test API

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import mongoose from 'mongoose';
+import { Router } from "express";
+import mongoose from "mongoose";
 const { ObjectId } = mongoose.Types;
 // import { ClerkExpressWithAuth } from "@clerk/clerk-sdk-node"
 
-import multer from 'multer';
+import multer from "multer";
 const upload = multer({ dest: "uploads/" });
 
 import { createUser, readUserById } from "../models/User.js";
@@ -11,22 +11,40 @@ import { createCourse, getCourseFiles } from "../models/Course.js";
 import { createFile } from "../models/File.js";
 const router = Router();
 
+router.get("/test", async (req, res, next) => {
+   try {
+      res.status(200).send({
+         message: "This is the test message from the server",
+      });
+   } catch (err) {
+      res.status(404).send({
+         ERROR: "Error: the page does not exist",
+      });
+      next();
+   }
+});
+
 /*
 Create a new file entry 
   Name
   Courses
 */
-router.post("/newFile", upload.single("file"), async (req, res, next) => {
-  try {
-    const filePath = req.file.path;
+router.post(
+   "/newFile",
+   upload.single("file"),
+   async (req, res, next) => {
+      try {
+         const filePath = req.file.path;
 
-    res.status(201).send({ message: "File uploaded successfully" });
-  } catch (err) {
-    res.status(404).send({ ERROR: "Error getting the file" });
-    next();
-  }
-});
-
+         res.status(201).send({
+            message: "File uploaded successfully",
+         });
+      } catch (err) {
+         res.status(404).send({ ERROR: "Error getting the file" });
+         next();
+      }
+   }
+);
 
 /*
 Create a new user entry 
@@ -34,40 +52,38 @@ Create a new user entry
   Courses
 */
 router.post("/", async (req, res, next) => {
-    var body = null;
-    try {
+   var body = null;
+   try {
       const id = await createUser(body);
       res.status(201).send({ _id: id });
-    } catch (err) {
+   } catch (err) {
       res.status(404).send({ ERROR: "error creating new user" });
       next();
-    }
+   }
 });
-
-
 
 /*
 Fetch user data by user id gotten from clerk
 */
-router.get("/getCourses", async (req, res, next) => { // ClerkExpressWithAuth(),
-    const userId = req.body.userId; //req.auth.userId;
-    var user = null;
-    try {
+router.get("/getCourses", async (req, res, next) => {
+   // ClerkExpressWithAuth(),
+   const userId = req.body.userId; //req.auth.userId;
+   var user = null;
+   try {
       user = await readUserByClerkId(userId);
-    } catch (err) {
+   } catch (err) {
       res.status(404).send({ ERROR: "Cannot find user" });
       return;
-    }
-  
-    if (!user) {
-      res.status(404).send({ ERROR: "Cannot find user" });
-      return;
-    }
-  
-    res.status(200).send(user);
-    return;
-  });
+   }
 
+   if (!user) {
+      res.status(404).send({ ERROR: "Cannot find user" });
+      return;
+   }
+
+   res.status(200).send(user);
+   return;
+});
 
 /*
 Creates a new course (and a new file) under a user
@@ -76,128 +92,126 @@ Creates a new course (and a new file) under a user
 */
 
 router.post("/course", async (req, res, next) => {
-    var body = null;
-    try {
+   var body = null;
+   try {
       body = await integrationValidSchema.validateAsync(req.body);
-    } catch (err) {
+   } catch (err) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    // Check if userId's type is ObjectId
-    if (!ObjectId.isValid(req.body.userId)) {
+   }
+
+   // Check if userId's type is ObjectId
+   if (!ObjectId.isValid(req.body.userId)) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    try {
+   }
+
+   try {
       const id = await createIntegration(body);
       res.status(201).send({ _id: id });
-    } catch (err) {
+   } catch (err) {
       next();
-    }
-  });
-
+   }
+});
 
 /*
 Delete a course 
 */
 router.delete("/:userId/:courseId", async (req, res, next) => {
-    var body = null;
-    try {
+   var body = null;
+   try {
       body = await integrationValidSchema.validateAsync(req.body);
-    } catch (err) {
+   } catch (err) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    // Check if userId's type is ObjectId
-    if (!ObjectId.isValid(req.body.userId)) {
+   }
+
+   // Check if userId's type is ObjectId
+   if (!ObjectId.isValid(req.body.userId)) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    try {
+   }
+
+   try {
       const id = await createIntegration(body);
       res.status(201).send({ _id: id });
-    } catch (err) {
+   } catch (err) {
       next();
-    }
-  });
+   }
+});
 
 /*
 Fetch course data by id
 */
 router.get("/:userId/:courseId", async (req, res, next) => {
-    const userId = req.params.userId;
-    var user = null;
-    try {
+   const userId = req.params.userId;
+   var user = null;
+   try {
       user = await readUserById(userId);
-    } catch (err) {
+   } catch (err) {
       res.status(404).send({ ERROR: "Cannot found user" });
       return;
-    }
-  
-    if (!user) {
+   }
+
+   if (!user) {
       res.status(404).send({ ERROR: "Cannot found user" });
       return;
-    }
-  
-    res.status(200).send(user);
-    return;
+   }
+
+   res.status(200).send(user);
+   return;
 });
 
 /*
 Create a new file for a course
 */
 router.post("/:userId/:courseId/file", async (req, res, next) => {
-    var body = null;
-    try {
+   var body = null;
+   try {
       body = await integrationValidSchema.validateAsync(req.body);
-    } catch (err) {
+   } catch (err) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    // Check if userId's type is ObjectId
-    if (!ObjectId.isValid(req.body.userId)) {
+   }
+
+   // Check if userId's type is ObjectId
+   if (!ObjectId.isValid(req.body.userId)) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    try {
+   }
+
+   try {
       const id = await createIntegration(body);
       res.status(201).send({ _id: id });
-    } catch (err) {
+   } catch (err) {
       next();
-    }
-  });
+   }
+});
 
 /*
 Delete a file from a course
 */
 router.delete("/:userId/:courseId/file", async (req, res, next) => {
-    var body = null;
-    try {
+   var body = null;
+   try {
       body = await integrationValidSchema.validateAsync(req.body);
-    } catch (err) {
+   } catch (err) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    // Check if userId's type is ObjectId
-    if (!ObjectId.isValid(req.body.userId)) {
+   }
+
+   // Check if userId's type is ObjectId
+   if (!ObjectId.isValid(req.body.userId)) {
       res.status(400).send({ ERROR: "Invalid body" });
       return;
-    }
-  
-    try {
+   }
+
+   try {
       const id = await createIntegration(body);
       res.status(201).send({ _id: id });
-    } catch (err) {
+   } catch (err) {
       next();
-    }
-  });
+   }
+});
 
 export default router;
-
